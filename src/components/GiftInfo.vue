0b620740408bc2f21c7b78cc6975d4f46552ba97
SissYo <stanimira.yovcheva@abv.bg>
2021-03-12 09:59:00 +0200

    Update GiftInfo.vue

--- src/components/GiftInfo.vue
+++ src/components/GiftInfo.vue
@@ -1,204 +1,85 @@
 <template>
-  <ValidationObserver ref="giftForm">
-    <div id="gift-form" class="section">
-      <h3 class="has-text-weight-semibold gift-title">Gift information</h3>
-      <b-field
-        id="amount-field"
-        label="Select gift amount"
-        custom-class="has-text-dark has-text-weight-medium"
-        :class="[form.amount == 'custom' ? 'mb-4' : 'parent-field']"
-        grouped
-        group-multiline
-      >
-        <b-radio-button
-          v-for="(item, index) in amounts"
-          :key="index"
-          v-model="form.amount"
-          :native-value="item.value"
-          type="is-info"
-          class="mr-3 radio-button has-text-dark has-text-weight-medium"
-          >{{ item.label }}</b-radio-button
+  <div>
+    <ValidationObserver ref="giftForm">
+      <div id="gift-form" class="section">
+        <h3 class="has-text-weight-semibold gift-title">Gift information</h3>
+        <b-field
+          id="amount-field"
+          label="Select gift amount"
+          custom-class="has-text-dark has-text-weight-medium"
+          :class="[form.amount == 'custom' ? 'mb-4' : 'parent-field']"
+          grouped
+          group-multiline
         >
-      </b-field>
-      <ValidationProvider
-        v-if="form.amount == 'custom'"
-        rules="required"
-        name="gift amount"
-        v-slot="{ errors }"
-      >
-        <b-field id="custom-amount-field" class="parent-field">
-          <p class="control">
-            <span class="button is-static has-background-white">$</span>
-          </p>
-          <b-input
-            v-model="form.customAmount"
-            placeholder="Enter custom amount"
-          ></b-input>
+          <b-radio-button
+            v-for="(item, index) in amounts"
+            :key="index"
+            v-model="form.amount"
+            :native-value="item.value"
+            type="is-info"
+            class="mr-3 radio-button has-text-dark has-text-weight-medium"
+            >{{ item.label }}</b-radio-button
+          >
         </b-field>
-        <span
-          v-for="error in errors"
-          :key="error.id"
-          class="help has-text-danger custom-amount-error"
-          >{{ error }}</span
-        >
-      </ValidationProvider>
-      <b-field
-        label="Delivery type"
-        class="mt-5 parent-field"
-        custom-class="has-text-dark has-text-weight-medium"
-      >
-        <b-radio
-          v-for="(item, index) in deliveryTypes"
-          :key="index"
-          v-model="form.deliveryType"
-          :native-value="item.value"
-          type="is-info"
-          class="mr-5 has-text-dark has-text-weight-normal"
-          >{{ item.label }}</b-radio
-        >
-      </b-field>
-      <b-field
-        v-if="form.deliveryType == 'gift'"
-        grouped
-        group-multiline
-        class="is-flex is-justify-content-space-evenly"
-      >
         <ValidationProvider
+          v-if="form.amount == 'custom'"
           rules="required"
-          name="sender name"
+          name="gift amount"
           v-slot="{ errors }"
         >
-          <b-field
-            label="Sender name"
-            class="parent-field"
-            custom-class="has-text-dark has-text-weight-medium"
-            expanded
-          >
-            <b-input
-              v-model="form.senderName"
-              placeholder="Enter sender name"
-              expanded
-            ></b-input>
-          </b-field>
-          <span
-            v-for="error in errors"
-            :key="error.id"
-            class="help has-text-danger mb-5"
-            >{{ error }}</span
-          >
-        </ValidationProvider>
-        <ValidationProvider
-          rules="required"
-          name="recipient name"
-          v-slot="{ errors }"
-        >
-          <b-field
-            label="Recipient name"
-            class="parent-field"
-            custom-class="has-text-dark has-text-weight-medium"
-            expanded
-          >
-            <b-input
-              v-model="form.recipientName"
-              placeholder="Enter recipient name"
-              expanded
-            ></b-input>
-          </b-field>
-          <span
-            v-for="error in errors"
-            :key="error.id"
-            class="help has-text-danger mb-5"
-            >{{ error }}</span
-          >
-        </ValidationProvider>
-      </b-field>
-      <b-field grouped group-multiline>
-        <ValidationProvider
-          rules="required"
-          name="recipient email"
-          v-slot="{ errors }"
-        >
-          <b-field
-            label="Recipient email"
-            class="parent-field"
-            custom-class="has-text-dark has-text-weight-medium"
-          >
-            <b-input
-              v-model="form.recipientEmail"
-              placeholder="Enter recipient email"
-              expanded
-            ></b-input>
-          </b-field>
-          <span
-            v-for="error in errors"
-            :key="error.id"
-            class="help has-text-danger mb-5"
-            >{{ error }}</span
-          >
-        </ValidationProvider>
-        <ValidationProvider
-          rules="required"
-          name="recipient phone"
-          v-slot="{ errors }"
-        >
-          <b-field
-            label="Recipient phone"
-            class="parent-field"
-            custom-class="has-text-dark has-text-weight-medium"
-          >
+          <b-field id="custom-amount-field" class="parent-field">
             <p class="control">
-              <span class="button is-static has-background-white">+65</span>
+              <span class="button is-static has-background-white">$</span>
             </p>
             <b-input
-              v-model="form.recipientPhone"
-              placeholder="Enter recipient phone"
-              expanded
+              v-model="form.customAmount"
+              placeholder="Enter custom amount"
             ></b-input>
           </b-field>
           <span
             v-for="error in errors"
             :key="error.id"
-            class="help has-text-danger mb-5"
+            class="help has-text-danger custom-amount-error"
             >{{ error }}</span
           >
         </ValidationProvider>
-      </b-field>
-      <div v-if="form.deliveryType == 'gift'">
         <b-field
-          label="Delivery time"
+          label="Delivery type"
+          class="mt-5 parent-field"
           custom-class="has-text-dark has-text-weight-medium"
-          class="parent-field"
         >
           <b-radio
-            v-for="(item, index) in deliveryTimes"
+            v-for="(item, index) in deliveryTypes"
             :key="index"
-            v-model="form.deliveryTime"
+            v-model="form.deliveryType"
             :native-value="item.value"
             type="is-info"
             class="mr-5 has-text-dark has-text-weight-normal"
             >{{ item.label }}</b-radio
           >
         </b-field>
-        <b-field v-if="form.deliveryTime == 'custom'" grouped group-multiline>
+        <b-field
+          v-if="form.deliveryType == 'gift'"
+          grouped
+          group-multiline
+          class="is-flex is-justify-content-space-evenly"
+        >
           <ValidationProvider
             rules="required"
-            name="delivery date"
+            name="sender name"
             v-slot="{ errors }"
           >
             <b-field
-              label="Delivery date"
+              label="Sender name"
               class="parent-field"
               custom-class="has-text-dark has-text-weight-medium"
+              expanded
             >
-              <b-datepicker
-                placeholder="Select delivery date"
-                icon="calendar-today"
-                :min-date="new Date()"
-                editable
-                class="has-icons-right"
-                v-model="form.deliveryDate"
-              >
-              </b-datepicker>
+              <b-input
+                v-model="form.senderName"
+                placeholder="Enter sender name"
+                expanded
+              ></b-input>
             </b-field>
             <span
               v-for="error in errors"
@@ -207,55 +88,178 @@
               >{{ error }}</span
             >
           </ValidationProvider>
-          <b-field
-            id="period-field"
-            label="Period"
-            class="parent-field"
-            custom-class="has-text-dark has-text-weight-medium"
+          <ValidationProvider
+            rules="required"
+            name="recipient name"
+            v-slot="{ errors }"
           >
-            <b-radio-button
-              v-for="(item, index) in periods"
+            <b-field
+              label="Recipient name"
+              class="parent-field"
+              custom-class="has-text-dark has-text-weight-medium"
+              expanded
+            >
+              <b-input
+                v-model="form.recipientName"
+                placeholder="Enter recipient name"
+                expanded
+              ></b-input>
+            </b-field>
+            <span
+              v-for="error in errors"
+              :key="error.id"
+              class="help has-text-danger mb-5"
+              >{{ error }}</span
+            >
+          </ValidationProvider>
+        </b-field>
+        <b-field grouped group-multiline>
+          <ValidationProvider
+            rules="required"
+            name="recipient email"
+            v-slot="{ errors }"
+          >
+            <b-field
+              label="Recipient email"
+              class="parent-field"
+              custom-class="has-text-dark has-text-weight-medium"
+            >
+              <b-input
+                v-model="form.recipientEmail"
+                placeholder="Enter recipient email"
+                expanded
+              ></b-input>
+            </b-field>
+            <span
+              v-for="error in errors"
+              :key="error.id"
+              class="help has-text-danger mb-5"
+              >{{ error }}</span
+            >
+          </ValidationProvider>
+          <ValidationProvider
+            rules="required"
+            name="recipient phone"
+            v-slot="{ errors }"
+          >
+            <b-field
+              label="Recipient phone"
+              class="parent-field"
+              custom-class="has-text-dark has-text-weight-medium"
+            >
+              <p class="control">
+                <span class="button is-static has-background-white">+65</span>
+              </p>
+              <b-input
+                v-model="form.recipientPhone"
+                placeholder="Enter recipient phone"
+                expanded
+              ></b-input>
+            </b-field>
+            <span
+              v-for="error in errors"
+              :key="error.id"
+              class="help has-text-danger mb-5"
+              >{{ error }}</span
+            >
+          </ValidationProvider>
+        </b-field>
+        <div v-if="form.deliveryType == 'gift'">
+          <b-field
+            label="Delivery time"
+            custom-class="has-text-dark has-text-weight-medium"
+            class="parent-field"
+          >
+            <b-radio
+              v-for="(item, index) in deliveryTimes"
               :key="index"
-              v-model="form.period"
+              v-model="form.deliveryTime"
               :native-value="item.value"
               type="is-info"
-              class="mr-3 radio-button has-text-dark has-text-weight-medium"
-              >{{ item.label }}</b-radio-button
+              class="mr-5 has-text-dark has-text-weight-normal"
+              >{{ item.label }}</b-radio
             >
           </b-field>
-        </b-field>
-        <ValidationProvider
-          rules="required"
-          name="gift message"
-          v-slot="{ errors }"
-        >
-          <b-field
-            label="Gift message"
-            custom-class="has-text-dark has-text-weight-medium"
-            class="parent-field"
-          >
-            <b-input
-              v-model="form.message"
-              type="textarea"
-              class="is-info"
-              placeholder="Enter gift message"
-            ></b-input>
+          <b-field v-if="form.deliveryTime == 'custom'" grouped group-multiline>
+            <ValidationProvider
+              rules="required"
+              name="delivery date"
+              v-slot="{ errors }"
+            >
+              <b-field
+                label="Delivery date"
+                class="parent-field"
+                custom-class="has-text-dark has-text-weight-medium"
+              >
+                <b-datepicker
+                  placeholder="Select delivery date"
+                  icon="calendar-today"
+                  :min-date="new Date()"
+                  editable
+                  class="has-icons-right"
+                  v-model="form.deliveryDate"
+                >
+                </b-datepicker>
+              </b-field>
+              <span
+                v-for="error in errors"
+                :key="error.id"
+                class="help has-text-danger mb-5"
+                >{{ error }}</span
+              >
+            </ValidationProvider>
+            <b-field
+              id="period-field"
+              label="Period"
+              class="parent-field"
+              custom-class="has-text-dark has-text-weight-medium"
+            >
+              <b-radio-button
+                v-for="(item, index) in periods"
+                :key="index"
+                v-model="form.period"
+                :native-value="item.value"
+                type="is-info"
+                class="mr-3 radio-button has-text-dark has-text-weight-medium"
+                >{{ item.label }}</b-radio-button
+              >
+            </b-field>
           </b-field>
-          <span
-            v-for="error in errors"
-            :key="error.id"
-            class="help has-text-danger"
-            >{{ error }}</span
+          <ValidationProvider
+            rules="required"
+            name="gift message"
+            v-slot="{ errors }"
           >
-        </ValidationProvider>
+            <b-field
+              label="Gift message"
+              custom-class="has-text-dark has-text-weight-medium"
+              class="parent-field"
+            >
+              <b-input
+                v-model="form.message"
+                type="textarea"
+                class="is-info"
+                placeholder="Enter gift message"
+              ></b-input>
+            </b-field>
+            <span
+              v-for="error in errors"
+              :key="error.id"
+              class="help has-text-danger"
+              >{{ error }}</span
+            >
+          </ValidationProvider>
+        </div>
+        <b-field class="is-flex is-justify-content-flex-end parent-field">
+          <b-button class="is-info" @click="sendGift">Send</b-button>
+        </b-field>
       </div>
-      <b-field class="is-flex is-justify-content-flex-end parent-field">
-        <b-button class="is-info" @click="sendGift">Send</b-button>
-      </b-field>
-    </div>
-  </ValidationObserver>
+    </ValidationObserver>
+    <gift-card-submit-info :data="form" ref="cardModal"></gift-card-submit-info>
+  </div>
 </template>
 <script>
+import GiftCardSubmitInfo from "@/components/GiftCardSubmitInfo";
 export default {
   name: "GitInfo",
   data() {
@@ -331,6 +335,7 @@ export default {
       ],
     };
   },
+  components: { GiftCardSubmitInfo },
   methods: {
     sendGift() {
       this.$refs.giftForm.validate().then((valid) => {
@@ -340,6 +345,8 @@ export default {
             type: "is-danger",
           });
         }
+
+        this.$refs.cardModal.openModal();
       });
     },
   },
