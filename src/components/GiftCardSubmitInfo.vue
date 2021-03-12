ef6fe92964459d858f6f765634c600dd3dc0d8a0
SissYo <stanimira.yovcheva@abv.bg>
2021-03-12 09:58:39 +0200

    Update GiftCardSubmitInfo.vue

--- src/components/GiftCardSubmitInfo.vue
+++ src/components/GiftCardSubmitInfo.vue
@@ -1,7 +1,35 @@
 <template>
   <section>
-    <b-modal v-model="isActive">
-      <div class="modal-card"></div>
+    <b-modal :active.sync="isActive" has-modal-card :on-cancel="closeModal">
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Gift card information</p>
+        </header>
+        <section class="modal-card-body">
+          <div>
+            Gift amount:
+            {{ data.amount == "custom" ? data.customAmount : data.amount }}
+          </div>
+          <div>Delivery type: {{ data.deliveryType }}</div>
+          <div v-if="data.deliveryType == 'gift'">
+            <div>Sender name: {{ data.senderName }}</div>
+            <div>Recipient name: {{ data.recipientName }}</div>
+          </div>
+          <div>Recipient email: {{ data.recipientEmail }}</div>
+          <div>Recipient phone: {{ data.recipientPhone }}</div>
+          <div>
+            Delivery date:
+            {{
+              data.deliveryTime == "custom" && data.deliveryDate
+                ? `${data.deliveryDate.toDateString()}, ${data.period}`
+                : data.deliveryTime
+            }}
+          </div>
+          <div v-if="data.deliveryType == 'gift'">
+            Gift message: {{ data.message }}
+          </div>
+        </section>
+      </div>
     </b-modal>
   </section>
 </template>
@@ -13,6 +41,15 @@ export default {
     return {
       isActive: false,
     };
+  },
+  props: ["data"],
+  methods: {
+    openModal() {
+      this.isActive = true;
+    },
+    closeModal() {
+      this.isActive = false;
+    },
   },
 };
 </script>
